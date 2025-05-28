66739d19354399b0ce2cd8e958af87e54499a7ee
Raúl Corvillo <raul.corvillo@agilecontent.com>
2025-05-28 10:45:06 +0200

    Update .gitignore to include additional IDE and build system files

---
+++ src/main/java/com/agiletv/streamFlix/StreamFlixApplication.java
@@ -0,0 +1,13 @@
+package com.agiletv.streamFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamFlixApplication.class, args);
+	}
+
+}
